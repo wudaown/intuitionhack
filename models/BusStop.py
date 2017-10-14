d76f6992277462ed9ce1f0f8fe1f71573fc2d6b2
Zhang Kaishuo <zhangks98@gmail.com>
2017-10-15 00:18:53 +0800

    filter fake stops

--- models/BusStop.py
+++ models/BusStop.py
@@ -67,6 +67,8 @@ def distance(orginPos, destPos, method):
 def get_nearest_bus_stop(lon,lat,stops,method):
 	mindis = math.inf
 	for st in stops:
+		if st.fake:
+			continue
 		try:
 			dis = distance((lon,lat),(st["lon"],st["lat"]),method)
 			if dis < mindis:
