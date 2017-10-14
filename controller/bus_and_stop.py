05080935690a3f497fc5973c63605b4ca08c0887
OrigenesZhang <zhangbin199807@gmail.com>
2017-10-14 22:49:18 +0800

    in

--- controller/bus_and_stop.py
+++ controller/bus_and_stop.py
@@ -1,5 +1,6 @@
 import json
 from models.BusStop import *
+from controller.getBusData import *
 def init_red():
 	with open(r"BusStopRed.json", "r") as stop_r:
 		stop_red=json.load(stop_r)
@@ -9,9 +10,24 @@ def init_red():
 		red_line_stops=insert(red_line_stops, Index, stop["lon"], stop["lat"], "RED", False, stop["code"], stop["title"])
 		Index+=1
 	red_line_stops=insert(red_line_stops, Index, stop_red[0]["lon"], stop_red[0]["lat"], "RED", False, stop_red[0]["code"], stop_red[0]["title"])
+	with open(r"mapdelta.json", "r") as fake_r:
+		fake_stop_red=json.load(fake_r)
+	Index=0
+	prev_str=""
+	Code=-1
+	for fake_stop in fake_stop_red:
+		#print(fake_stop)
+		if fake_stop["title"][:-1]!=prev_str:
+			Index+=1
+		red_line_stops=insert(red_line_stops, Index, fake_stop["lon"], fake_stop["lat"], "RED", True, Code, fake_stop["title"])
+		Code-=1
+		prev_str=fake_stop["title"][:-1]
+		Index+=1
 	dist=[]
 	for i in range(0, len(red_line_stops)-1):
 		dist.append(distance(red_line_stops[i].pos, red_line_stops[i+1].pos))
+	for stop in red_line_stops:
+		print(stop.name, stop.fake, stop.code)
 	return red_line_stops, dist
 #red_line_stops, dist=init_red()
 #print(dist)
@@ -22,4 +38,11 @@ def init_blue():
 
 def bus_queue(colour, code):
 	ETA=[]
+	'''
+	if colour=="RED":
+		b=Bus()
+		b.update_response()
+		bus=b.get_red()
+		for buses in bus:
+	'''
 	return ETA
